1a99fcbd5f47ab4337252c532e3d044a88df1462
budparr <budparr@sonnetmedia.net>
2014-09-21 08:06:33 -0400

    update nav bg color
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- assets/js/concatenate-scripts.js
+++ assets/js/concatenate-scripts.js
@@ -4,7 +4,52 @@ layout: null
 {% include scripts/jquery.min.js %}
 {% include scripts/jquery_cycle/jquery.cycle2.min.js %}
 {% include scripts/links-new-window.js %}
+{% include scripts/listjs/dist/list.min.js %}
 
+
+  var options = {
+      valueNames: [ 'name', 'slug' , 'category', 'tagline', 'categories', 'date'],
+      listClass: 'list-filter',
+      page: 2000 // how many items should first show up on the page - affects performance
+      // add tagline, or option for it?
+  };
+
+
+  var authorList = new List('container-filter', options);
+
+
+if (document.getElementById("filter-news")) {
+  document.getElementById("filter-news").onclick=function(){
+          authorList.filter(function(item) {
+             if (item.values().category == "news" ) {
+               return true;
+               } else {
+               return false;
+            }
+        });
+      };
+    }
+
+if (document.getElementById("filter-opinion")) {
+  document.getElementById("filter-opinion").onclick=function(){
+          authorList.filter(function(item) {
+             if (item.values().category == "opinion" ) {
+               return true;
+               } else {
+               return false;
+            }
+        });
+      };
+    }
+
+
+
+//and clear the filters
+if (document.getElementById("filter-clear")) {
+   document.getElementById("filter-clear").onclick=function(){
+       authorList.filter();
+      };
+    }
 
 
   $('.js-menu-trigger').on('click touchstart', function(e){
